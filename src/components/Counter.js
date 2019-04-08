9df5d2cb34ee18919abfb3b9fb4167d8b51bc478
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-04-08 17:04:52 -0500

    stretch goals finished

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,20 +3,31 @@ import { connect } from 'react-redux';
 import { increment, decrement } from '../actions';
 
 class Counter extends Component {
-    incrementIfOdd = () => {
+    componentDidMount() {
+        this.incrementAsync();
+    }
+    
+    componentWillReceiveProps(nexProps) {
+        this.incrementIfOdd(nexProps.count);
+    }
+
+    incrementIfOdd = count => {
         // Stretch Problem: Implement an increment function that
         // only increments if the counter value is odd
+        if(count % 2 !== 0)  this.props.increment();
     };
 
     incrementAsync = () => {
         // Stretch Problem: Implement an increment function that
         // increments after waiting for one second
+        setTimeout(_ => this.props.increment(),1000);
     };
 
     render() {
         // Fill in the two button onClick methods
         // Upon clicking these buttons, the count
         // should decrement or increment accordingly
+        
         console.log(this.props);
         return (
             <p>
